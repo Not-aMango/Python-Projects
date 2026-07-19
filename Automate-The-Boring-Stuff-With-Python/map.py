7f828419ddeca08a6f5a9564d12bd6d2d1884362
Not-aMango <piyush9839297@gmail.com>
2026-07-19 12:49:13 +0530

    Open Maps from Terminal

---
+++ Automate-The-Boring-Stuff-With-Python/map.py
@@ -0,0 +1,10 @@
+import webbrowser, sys, pyperclip
+
+url = 'https://www.google.com/maps/search/'
+
+if len(sys.argv) > 1:
+    location = url + "+".join(sys.argv[1:])
+else:
+    location = url + pyperclip.paste().replace(" ", "+")
+
+webbrowser.open(location)
