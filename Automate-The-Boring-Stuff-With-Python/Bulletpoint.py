d037c3e6fb441b837bf151c89e62f33496701075
Not-aMango <piyush9839297@gmail.com>
2026-07-20 11:16:54 +0530

    Made it terminal Based

--- Automate-The-Boring-Stuff-With-Python/Bulletpoint.py
+++ Automate-The-Boring-Stuff-With-Python/Bulletpoint.py
@@ -1,6 +1,18 @@
-import pyperclip
-a = pyperclip.paste().split('\n')
-c=''
-for i in a:
-    c+='• '+i+'\n'
-pyperclip.copy(c)
+#!/home/notamango/PyCharmMiscProject/Python_files/.venv/bin/python3
+
+import pyperclip, sys
+
+points = pyperclip.paste().split('\n')
+sentences = ''
+
+def yes():
+    global sentences
+    for sentence in points: sentences = sentences + f'• {sentence}\n'
+def no():
+    global sentences
+    for sentence in points: sentences = sentences + sentence[2:]
+
+if sys.argv[1] == '-a': yes()
+elif sys.argv[1] == '-r': no()
+
+pyperclip.copy(sentences)
