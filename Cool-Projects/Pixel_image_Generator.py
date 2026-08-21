31caddb76496aad7ea3b2b2445f0b18bea3f643e
Not-aMango <piyush9839297@gmail.com>
2026-08-21 19:40:47 +0530

    RGB format to RGBA

--- Cool-Projects/Pixel_image_Generator.py
+++ Cool-Projects/Pixel_image_Generator.py
@@ -4,14 +4,14 @@ from pathlib import Path
 print('This program pixelates any given image to make it look like pixelated retro-style image.\n')
 
 directory = input("Enter an absolute image dir. : ")
-image = Image.open(directory)
+image = Image.open(directory).convert('RGBA')
 size = image.size
 
 d = int(input('Enter pixel density(ideal 10 -> less number means more detailed image) : '))
 
 def pixelify(image):
     print('\nProcessing Image\n')
-    pixel_image = Image.new('RGB', size)
+    pixel_image = Image.new('RGBA', size)
 
     for x in range(0, size[0] - (d-1), d):
         for y in range(0, size[1] - (d-1), d):
@@ -20,7 +20,7 @@ def pixelify(image):
             stat = ImageStat.Stat(crop)
             mean = tuple(int(x) for x in stat.mean)
 
-            tile = Image.new('RGB',crop.size,mean)
+            tile = Image.new('RGBA',crop.size,mean)
 
             pixel_image.paste(tile,(x,y))
     print('Done\n')
