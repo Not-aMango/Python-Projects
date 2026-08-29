b49e33f597632934bf9c74ec60d7ce0790d8aff2
Not-aMango <piyush9839297@gmail.com>
2026-08-29 12:06:21 +0530

    Still WOrking on it

--- Cool-Projects/MorseCode.py
+++ Cool-Projects/MorseCode.py
@@ -1,3 +1,8 @@
+import customtkinter as ctk
+app = ctk.CTk()
+ctk.set_appearance_mode('dark')
+app.geometry('900x1000')
+
 morse = {
 "A": ".-",
 "B": "-...",
@@ -27,25 +32,58 @@ morse = {
 "Z": "--..",
 " ": "/"
 }
-def generator():
-    print("\n******** Convert String to MorseCode ********")
-    word = input("Enter a Word/Statement: ").upper()
-    print("\nMorse Code:")
-    for letter in word:
-        if letter in morse: print(morse[letter],end= " ")
-        else: print(letter,end= " ")
-def reverser():
-    print("\n******** Convert MorseCode to String ********")
-    word = input("Enter a Morse Code: ")
-    word  = word.strip().split(" ")
-    rev = {}
-    for key,value in morse.items(): rev.update({value:key})
-    print("\nString:")
-    for letter in word:
-        if letter in rev: print(rev[letter],end="")
-        else: print(letter.strip(),end="")
-print("Press \"G\" to generate a Morse Code\n","OR".center(30),"\n  Press \"D\" to decode a Morse Code")
-choice = input(": ").lower()
-if choice == "g":generator()
-elif choice == "d":reverser()
-else: print("Invalid Input")
+class MorseCode():
+    def __init__(self):
+        #title
+        self.title = ctk.CTkLabel(app,text='𝕄𝕠𝕣𝕤𝕖ℂ𝕠𝕕𝕖 𝔾𝕖𝕟𝕖𝕣𝕒𝕥𝕠𝕣',font=ctk.CTkFont(size=50,weight='bold'))
+        self.bar = ctk.CTkProgressBar(app,progress_color='magenta',width=700)
+        self.title.place(relx=0.5,rely=0.02,anchor='n')
+        self.bar.place(relx=0.5,rely=0.08,anchor='center')
+
+        #tab
+        self.tab = ctk.CTkTabview(app,width=800,height=800,segmented_button_font=ctk.CTkFont(size=25),fg_color='transparent')
+        self.tab.add('MorseCode to Text')
+        self.tab.add('Text to MorseCode')
+        self.tab1 = self.tab.tab('MorseCode to Text')
+        self.tab2 = self.tab.tab('Text to MorseCode')
+        self.tab.place(relx=0.5,rely=0.12,anchor='n')
+
+        #entry tab1
+        self.entry1 = ctk.CTkTextbox(self.tab1,width=670,font=ctk.CTkFont(size=20),fg_color='#343638',border_width=2,border_color='#53585b')
+        self.entry1.place(relx=0.5,rely=0.1,anchor='n')
+
+        #entry tab2
+        self.entry2 = ctk.CTkTextbox(self.tab2, width=670, font=ctk.CTkFont(size=20), fg_color='#343638', border_width=2,border_color='#53585b')
+        self.entry2.place(relx=0.5, rely=0.1, anchor='n')
+
+        #convert button
+        self.convert_button = ctk.CTkButton(self.tab,text='Convert',font=ctk.CTkFont(size=40,weight='bold'),width=180,height=45,command=self.morse_to_text)
+        self.convert_button.place(relx=0.5,rely=0.52,anchor='center')
+
+        #display tab1
+        self.display_box1 = ctk.CTkTextbox(self.tab1,font=ctk.CTkFont(size=20),width=670,fg_color='#343638',border_width=2,border_color='#53585b')
+        self.display_box1.place(relx=0.5,rely=0.6,anchor='n')
+
+        #display tab2
+        self.display_box2 = ctk.CTkTextbox(self.tab2, font=ctk.CTkFont(size=20), width=670, fg_color='#343638',border_width=2, border_color='#53585b')
+        self.display_box2.place(relx=0.5, rely=0.6, anchor='n')
+
+    def morse_to_text(self):
+        self.display_box1.insert(0.0,'')
+        word = self.entry1.get(0.0,'end').upper()
+        morsecode = ''
+        for letter in word:
+            if letter in morse:
+                morsecode = morsecode + morse[letter] + " "
+            else:
+                morsecode = morsecode + letter + ' '
+        self.display_box1.insert(0.0,morsecode)
+
+
+
+MorseCode()
+
+
+app.mainloop()
+
+#⇆
